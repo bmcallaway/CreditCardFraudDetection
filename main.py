45e395ef1cee6d1ce6189517d51d050f2f3fa5ff
bmcallaway <bccallaw@asu.edu>
2026-04-27 01:32:24 -0700

    updated cors

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ model = FraudDetectionModel()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
